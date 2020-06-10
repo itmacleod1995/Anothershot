c66bcb4f42cd197011d118a0faeff04af25b32a4
itmacleod1995 <imacleod@mail.umw.edu>
2020-06-10 19:36:43 -0400

    Added comments to function

--- countNaFunc.R
+++ countNaFunc.R
@@ -1,10 +1,16 @@
+#input column of values from dataframe
 numberOfNa <- function(col){
+  #initialize count to be 0
   count <- 0
+  #for loop that runs through each value in the column
   for(i in col){
+    #if value, represented by i, is NA...
     if(is.na(i)){
+      #increment count by 1
       count = count + 1
     }
   }
   
   return(count)
+}
 }
