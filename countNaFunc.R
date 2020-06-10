b1c99a4e625cfcfdd2126e8b2c0be88b3464ea52
itmacleod1995 <imacleod@mail.umw.edu>
2020-06-10 19:54:07 -0400

    Returns percentage of complete

--- countNaFunc.R
+++ countNaFunc.R
@@ -1,16 +1,18 @@
 #input column of values from dataframe
-numberOfNa <- function(col){
-  #initialize count to be 0
-  count <- 0
+calcCompleteness <- function(col){
+  #initialized to be 0
+  numberOfNA <- 0
   #for loop that runs through each value in the column
   for(i in col){
     #if value, represented by i, is NA...
     if(is.na(i)){
       #increment count by 1
-      count = count + 1
+      numberOfNA = numberOfNA + 1
     }
   }
   
-  return(count)
-}
+  #return the % of complete values in the column
+  return((length(col) - numberOfNA) / length(col) * 100)
+  
+  
 }
